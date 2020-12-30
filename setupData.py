26a6b4d08f92a31e72c9f44bc37b8219a468fe29
jq3219 <jq3210@naver.com>
2020-12-30 13:41:40 +0900

    adopt multiThread

--- setupData.py
+++ setupData.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-     #이거 있어야 cmd에서 주석도 한글 처리 됨 (없으면 한글주석 에러)
+#나중에 멀티 스레드로 성능 개선 하던가 아님 그냥 코드 수정으로 성능 개선하던가 방식 선택 / 가능하면 코드수정 + 연도별로 멀티스레딩 할 수 있으면 좋을듯
 import pandas as pd
 import numpy as np
 import os
 import timeit, time
+import threading
 
 from tqdm import tqdm   #로딩바 확인을 위한 모듈
-
-start_time = timeit.default_timer() # 시작 시간 체크
 
 min_dataV = 0
 max_dataV = [0,0,0]
 
 #19년 20년 월별 데이터 생성
-data2019 = {}
-data2020 = {}
-seoul_subway_dataF = os.path.abspath('seoul_subway_data') + '/'
+data2019 = {'year' : 2019}
+data2020 = {'year' : 2020}
 
-def csv_to_dataFrame(year,dataY):
-    global min_dataV,max_dataV
+seoul_subway_dataF = os.path.abspath('seoul_subway_data') + '/'
+gu_sta_name = pd.read_csv(seoul_subway_dataF + 'Gu_StationNM.csv',encoding='utf-8')
+
+def csv_to_dataFrame(dataY):
+    start_time = timeit.default_timer() # 시작 시간 체크
+    # global min_dataV,max_dataV
+    global gu_sta_name
+    year = dataY['year']
     year = str(year)
-    
+    dataY.__delitem__('year')
+
     for csvF in os.listdir(seoul_subway_dataF + '{}'.format(year)):
         #19년도 데이터 안에 이미 데이터가 있으면 넘어가고 없으면 기본값으로 추가
         if not 'data' + csvF[-10:-4] in dataY.keys():
@@ -33,9 +39,7 @@ def csv_to_dataFrame(year,dataY):
             data = pd.read_csv(seoul_subway_dataF + year + '/' + csvF,encoding = 'EUC-KR',index_col=False)
         dataY['data' + csvF[-10:-4]] = data
         
-        
     c_dataY = dataY.copy()
-    
     #로딩 바 (진행상황 확인)
     c_dataY_keys_list = tqdm(c_dataY.keys(),desc = year + 'data : ')
 
@@ -51,8 +55,7 @@ def csv_to_dataFrame(year,dataY):
             
         dataY[dataYM]['유동인구수'] = dataY[dataYM]['승차총승객수'] + dataY[dataYM]['하차총승객수']
         
-        
-        newData_min = {'사용일자':'', '승차총승객수':min_dataV, '하차총승객수':min_dataV, '유동인구수':min_dataV}
+        # newData_min = {'사용일자':'', '승차총승객수':min_dataV, '하차총승객수':min_dataV, '유동인구수':min_dataV}
 
         #20190101 20190102 ...20190131
         dateKeys = dataY[dataYM].groupby('사용일자').indices.keys()
@@ -98,9 +101,7 @@ def csv_to_dataFrame(year,dataY):
             dataY['data{}'.format(i)]['사용일자'] = i
             #인구수는 최저 0으로 고정 최대는 데이터마다 다름
             #근데 이게 지금 작동이 안되네...
-            dataY['data{}'.format(i)] = dataY['data{}'.format(i)].append(newData_min,ignore_index=True)
-            
-#             print(dataY['data{}'.format(i)])
+            # dataY['data{}'.format(i)] = dataY['data{}'.format(i)].append(newData_min,ignore_index=True)
             
             #같은 노선 갯수 구해서 전체 합에서 노선 갯수만큼 나눠주기
             b_c = dataY['data{}'.format(i)].groupby('역명').count()[['승차총승객수','하차총승객수','유동인구수']]
@@ -116,48 +117,28 @@ def csv_to_dataFrame(year,dataY):
             
         dataY[dataYM] = t_dataY.groupby('역명').sum().rename_axis('역명').reset_index()
         dataY[dataYM]['사용일자'] = dataYM[4:]
-                
-#지도를 어떻게 표현할지에 대해 필요있을 수 있는 코드
-#(모든 월별 지도의 최대 레전드 값을 고정을 시켜놓으면 전체적으로 볼때는 알아보기 쉬우나 특정 몇몇 지역들이 지나치게 흐려짐)
-#(월별로 지도의 최대 레전드 값을 다르게 하면 색은 눈에 띄게 표현 가능하나 전체적인 지도를 볼 때는 계속해서 레전드의 최댓값을 확인할 필요가 잇음)
-#         if max_dataV[0] < dataF[dataY]['승차총승객수'].max():
-#             max_dataV[0] = dataF[dataY]['승차총승객수'].max()
-        
-#         if max_dataV[1] < dataF[dataY]['하차총승객수'].max():
-#             max_dataV[1] = dataF[dataY]['하차총승객수'].max()
-            
-#         if max_dataV[2] < dataF[dataY]['유동인구수'].max():
-#             max_dataV[2] = dataF[dataY]['유동인구수'].max()           
 
-csv_to_dataFrame(2019,data2019)
-csv_to_dataFrame(2020,data2020)
+        if not '구' in dataY[dataYM].columns:    
+            dataY[dataYM] = pd.merge(dataY[dataYM],gu_sta_name,on='역명',how = 'inner')
 
-terminate_time = timeit.default_timer() # 종료 시간 체크  
+            if True in dataY[dataYM].duplicated():
+                dataY[dataYM] = dataY[dataYM].drop_duplicates()
+                dataY[dataYM].reset_index(inplace=True,drop = True)
 
-gu_sta_name = pd.read_csv(seoul_subway_dataF + 'Gu_StationNM.csv',encoding='utf-8')
+        dataY[dataYM] = dataY[dataYM].sort_values(['사용일자','역명'],ascending = True).reset_index(drop = True)             
 
-def insert_gu(dataY):
-    #데이터에 구 추가
+    terminate_time = timeit.default_timer() # 종료 시간 체크  
+    print("{}data : %f초 걸렸습니다.".format(year) % (terminate_time - start_time))
 
-    dataY_keys_list = tqdm(dataY.keys(),desc = '잔여 데이터 처리 : ')
 
-    for dataYMD in dataY_keys_list:
-        if not '구' in dataY[dataYMD].columns:    
-            dataY[dataYMD] = pd.merge(dataY[dataYMD],gu_sta_name,on='역명',how = 'inner')
+data_list = [data2019,data2020]
 
-            if True in dataY[dataYMD].duplicated():
-                dataY[dataYMD] = dataY[dataYMD].drop_duplicates()
-                dataY[dataYMD].reset_index(inplace=True,drop = True)
-
-        dataY[dataYMD] = dataY[dataYMD].sort_values(['사용일자','역명'],ascending = True).reset_index(drop = True)             
-    
-insert_gu(data2019)
-insert_gu(data2020)
+for i in range(len(data_list)):
+    th1 = threading.Thread(target=csv_to_dataFrame, args = (data_list[i],))
+    th1.start()
 
 DATA = {}
 if not 'data2019' in DATA.keys():
     DATA['data2019'] = data2019
 if not 'data2020' in DATA.keys():
     DATA['data2020'] = data2020
-
-print("%f초 걸렸습니다." % (terminate_time - start_time))
