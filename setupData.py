2680c8fb2b270dd26e6137d50cdc9dbdb3163899
jq3219 <jq3210@naver.com>
2020-12-30 16:52:50 +0900

    adopt multithreading

--- setupData.py
+++ setupData.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-     #이거 있어야 cmd에서 주석도 한글 처리 됨 (없으면 한글주석 에러)
-#나중에 멀티 스레드로 성능 개선 하던가 아님 그냥 코드 수정으로 성능 개선하던가 방식 선택 / 가능하면 코드수정 + 연도별로 멀티스레딩 할 수 있으면 좋을듯
 import pandas as pd
 import numpy as np
 import os
@@ -8,8 +7,7 @@ import threading
 
 from tqdm import tqdm   #로딩바 확인을 위한 모듈
 
-min_dataV = 0
-max_dataV = [0,0,0]
+start_time = timeit.default_timer() # 시작 시간 체크
 
 #19년 20년 월별 데이터 생성
 data2019 = {'year' : 2019}
@@ -19,7 +17,6 @@ seoul_subway_dataF = os.path.abspath('seoul_subway_data') + '/'
 gu_sta_name = pd.read_csv(seoul_subway_dataF + 'Gu_StationNM.csv',encoding='utf-8')
 
 def csv_to_dataFrame(dataY):
-    start_time = timeit.default_timer() # 시작 시간 체크
     # global min_dataV,max_dataV
     global gu_sta_name
     year = dataY['year']
@@ -55,8 +52,6 @@ def csv_to_dataFrame(dataY):
             
         dataY[dataYM]['유동인구수'] = dataY[dataYM]['승차총승객수'] + dataY[dataYM]['하차총승객수']
         
-        # newData_min = {'사용일자':'', '승차총승객수':min_dataV, '하차총승객수':min_dataV, '유동인구수':min_dataV}
-
         #20190101 20190102 ...20190131
         dateKeys = dataY[dataYM].groupby('사용일자').indices.keys()
         
@@ -99,9 +94,6 @@ def csv_to_dataFrame(dataY):
             
             dataY['data{}'.format(i)] = dataY['data{}'.format(i)].reset_index()
             dataY['data{}'.format(i)]['사용일자'] = i
-            #인구수는 최저 0으로 고정 최대는 데이터마다 다름
-            #근데 이게 지금 작동이 안되네...
-            # dataY['data{}'.format(i)] = dataY['data{}'.format(i)].append(newData_min,ignore_index=True)
             
             #같은 노선 갯수 구해서 전체 합에서 노선 갯수만큼 나눠주기
             b_c = dataY['data{}'.format(i)].groupby('역명').count()[['승차총승객수','하차총승객수','유동인구수']]
@@ -127,16 +119,26 @@ def csv_to_dataFrame(dataY):
 
         dataY[dataYM] = dataY[dataYM].sort_values(['사용일자','역명'],ascending = True).reset_index(drop = True)             
 
-    terminate_time = timeit.default_timer() # 종료 시간 체크  
-    print("{}data : %f초 걸렸습니다.".format(year) % (terminate_time - start_time))
-
+    c_dataY_keys_list.clear()
+    c_dataY_keys_list.close()
 
 data_list = [data2019,data2020]
-
+#여러개 스레드 객체를 다루기 위한 스레드 리스트
+thread_list = []
+#스레드 병렬실행을 통해 2019,2020 등 한번에 data 생성
 for i in range(len(data_list)):
     th1 = threading.Thread(target=csv_to_dataFrame, args = (data_list[i],))
     th1.start()
+    #생성된 스레드 객체들 리스트에 담기
+    thread_list.append(th1)
 
+#모든 스레드가 끝날때까지 대기
+for th in thread_list:
+    th.join()
+    print()
+
+terminate_time = timeit.default_timer() # 종료 시간 체크  
+print("%f초 걸렸습니다." % (terminate_time - start_time))
 DATA = {}
 if not 'data2019' in DATA.keys():
     DATA['data2019'] = data2019
